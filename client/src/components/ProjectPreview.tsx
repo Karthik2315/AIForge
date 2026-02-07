de832f1f9e8c72faf0be992991bc52a4daadc52e
Karthik P <padiyil2004@gmail.com>
2026-02-07 12:54:05 +0530

    made the animation for loaderSteps and made the getCode functoin

--- client/src/components/ProjectPreview.tsx
+++ client/src/components/ProjectPreview.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect, useRef, useState } from 'react'
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
 import type { Project } from '../types';
 import { iframeScript } from '../assets/assets';
 import EditorPanel from './EditorPanel';
@@ -54,6 +54,24 @@ const ProjectPreview = forwardRef<ProjectPreviewRef,ProjectPreviewProps>(({proje
       return html + iframeScript;
     }
   }
+  useImperativeHandle(ref,()=>({
+    getCode : ()=>{
+      const doc = iframeRef.current?.contentDocument;
+      if(!doc) return undefined;
+      doc.querySelectorAll('.ai-selected-element,[data-ai-selected]').forEach((el)=>{
+        el.classList.remove('ai-selected-element');
+        el.removeAttribute('data-ai-selected');
+        (el as HTMLElement).style.outline = '';
+      })
+      const previewStyle = doc.getElementById('ai-preview-style');
+      if(previewStyle) previewStyle.remove();
+
+      const previewScript = doc.getElementById('ai-preview-script');
+      if(previewScript) previewScript.remove();
+      const html = doc.documentElement.outerHTML;
+      return html;
+    }
+  }))
   return (
     <div className='relative h-full bg-gray-900 flex-1 rounded-xl overflow-hidden max-sm:ml-2 no-scrollbar'>
       {
