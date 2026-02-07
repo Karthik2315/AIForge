77a9e3027c59f2a0d9d9571eeef8a223ae5f3ea8
Karthik P <padiyil2004@gmail.com>
2026-02-07 13:50:00 +0530

    remove loader2 import

--- client/src/pages/View.tsx
+++ client/src/pages/View.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { dummyProjects } from "../assets/assets";
-import { Loader2, Loader2Icon } from "lucide-react";
+import { Loader2Icon } from "lucide-react";
 import ProjectPreview from "../components/ProjectPreview";
 import type { Project } from "../types";
 
