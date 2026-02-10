4df6347ea88990341c7c273f255dd3276685119b
Karthik P <padiyil2004@gmail.com>
2026-02-11 00:24:20 +0530

    added the better auth ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,6 @@ import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import { Providers } from "./providers.tsx"
-import { A } from 'node_modules/@daveyplate/better-auth-ui/dist/auth-ui-provider-3JMiYGSS';
 
 
 createRoot(document.getElementById('root')!).render(
