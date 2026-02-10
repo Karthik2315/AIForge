379b7ef9452e8389e15df1254583675207e8fc93
Karthik P <padiyil2004@gmail.com>
2026-02-10 23:57:03 +0530

    added the better-auth ui provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
+import { Providers } from "./providers.tsx"
+import { A } from 'node_modules/@daveyplate/better-auth-ui/dist/auth-ui-provider-3JMiYGSS';
+
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </BrowserRouter>,
 )
