246ca834a1856d32fbac5e76d2944e4d1fa30f9c
Karthik P <padiyil2004@gmail.com>
2026-02-27 10:15:32 +0530

    setted the rate limiter and limit for express.json and rearranged the order of code lines

--- server/server.ts
+++ server/server.ts
@@ -3,21 +3,39 @@ import cors from "cors";
 import 'dotenv/config'
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
+import userRouter from './routes/userRouter.js';
+import rateLimit from "express-rate-limit";
 
 const app = express();
-
 const port = 3000;
 
+app.set("trust proxy", 1);
+
+app.use(express.json({ limit: '50mb' }));
+
 const corsOptions = {
-  origin:process.env.TRUSTED_ORIGIN?.split(',') || [],
-  credentials:true
-}
-app.use(cors(corsOptions))
+  origin: process.env.TRUSTED_ORIGIN?.split(',') || [],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests, please try again later."
+});
+
+app.use(limiter);
+
 app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use('/api/user', userRouter);
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+  res.send('Server is Live!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
