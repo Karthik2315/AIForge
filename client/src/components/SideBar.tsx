b61d565658c6df9edae39c1c8dadbb035df82e3c
Karthik P <padiyil2004@gmail.com>
2026-03-03 21:02:36 +0530

    add backend fetches and completed the functions

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -2,6 +2,8 @@ import  { useEffect, useRef, useState } from 'react'
 import type { Message, Project, Version } from '../types';
 import { BotIcon, EyeIcon, Loader2Icon, SendIcon, User2Icon } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import api from '@/configs/axios';
+import { toast } from 'sonner';
 
 interface SideBarProps {
   isMenuOpen : boolean;
@@ -15,16 +17,50 @@ const SideBar = ({isMenuOpen,project,setProject,isGenerating,setIsGenerating}:Si
   
   const messageRef = useRef<HTMLDivElement>(null);
   const [input,setInput] = useState<string>("");
+  const fetchProjects = async()=>{
+    try {
+      const {data} = await api.get(`/api/user/project/${project.id}`);
+      setProject(data.project);
+    } catch (error:unknown) {
+      console.log(error);
+    }
+  }
   const handleRollBack = async(versionId:string) => {
-    
+    try {
+      const confirm = window.confirm('Are you sure you want to rollback to this version?')
+      if(!confirm) return ;
+      setIsGenerating(true);
+      const {data} = await api.get(`/api/project/rollback/${project.id}/${versionId}`);
+      const { data:data2 } = await api.get(`/api/user/project/${project.id}`);
+      toast.success(data.message);
+      setProject(data2.project);
+      setIsGenerating(false);
+    } catch (error:unknown) {
+      console.log(error)
+    }
   }
 
   const handleRevisions = async(e:React.FormEvent) => {
     e.preventDefault();
-    setIsGenerating(true);
-    setTimeout(() => {
+    let interval: number | undefined;
+    try {
+      setIsGenerating(true);
+      interval = setInterval(async() => {
+        await fetchProjects();
+      }, 10000);
+      const {data} = await api.post(`/api/project/revision/${project.id}`,{
+        message:input
+      });
+      await fetchProjects();
+      toast.success(data.message);
+      setInput('');
+      clearInterval(interval);
       setIsGenerating(false);
-    }, 3000);
+    } catch (error:unknown) {
+      console.log(error);
+      setIsGenerating(false);
+      clearInterval(interval);
+    }
   }
 
   useEffect(()=>{
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -40,7 +40,12 @@ const Home = () => {
         </p>
 
         <form onSubmit={onSubmitHandler} className="bg-white/10 max-w-2xl w-full rounded-xl p-4 mt-10 border border-indigo-600/70 focus-within:ring-2 ring-indigo-500 transition-all">
-          <textarea onChange={e => setInput(e.target.value)} className="bg-transparent outline-none text-gray-300 resize-none w-full" rows={4} placeholder="Describe your presentation in details" required />
+          <textarea onChange={e => setInput(e.target.value)} onKeyDown={e => {
+            if(e.key === "Enter" && !e.shiftKey){
+              e.preventDefault();
+              onSubmitHandler(e as React.FormEvent)
+            }
+          }} className="bg-transparent outline-none text-gray-300 resize-none w-full" rows={4} placeholder="Describe your presentation in details" required />
           <button className="ml-auto flex items-center gap-2 bg-gradient-to-r from-[#CB52D4] to-indigo-600 rounded-md px-4 py-2 cursor-pointer hover:scale-105 active:scale-95 transition-all duration-300">
             {loading ? <>
               Creating <Loader2 className='size-4 animate-spin text-white'/>
